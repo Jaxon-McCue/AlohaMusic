16ce9ba23b7e1a4d9d463957633a8dba03207c97
Jaxon McCue <vm737839@southeast.edu>
2023-03-05 16:59:17 -0600

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { Audio, InterruptionModeAndroid, interruptionModeAndroid, InterruptionModeIOS, interruptionModeIOS } from "expo-av";
+import { Feather } from "@expo/vector-icons";
+import * as SplashScreen from "expo-splash-screen";
+import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
+
+export default class App extends Component {
+  state = {
+    isPlaying: false,
+    playbackInstance: null,
+    volume: 1.0,
+    isBuffering: false,
+  };
+
+  async componentDidMount() {
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      playThroughEarpieceAndroid: true,
+      //interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      interruptionModeIOS: InterruptionModeIOS.DoNotMix,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      //interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
+    });
+    this.loadAudio();
+  }
+
+  handlePlayPause = async () => {
+    const { isPlaying, playbackInstance } = this.state;
+    isPlaying
+      ? await playbackInstance.pauseAsync()
+      : await playbackInstance.playAsync();
+    this.setState({
+      isPlaying: !isPlaying,
+    });
+  };
+
+  onPlaybackStatusUpdate = (status) => {
+    this.setState({
+      isBuffering: status.isBuffering
+    });
+  }
+
+  async loadAudio() {
+    const playbackInstance = new Audio.Sound();
+		const status = {
+			shouldPlay: this.state.isPlaying,
+			volume: this.state.volume,
+    };
+    playbackInstance
+      .setOnPlaybackStatusUpdate(
+        this.onPlaybackStatusUpdate
+      );
+    await playbackInstance.loadAsync(require("./music/ukulele.mp3"), status, false);
+    this.setState({
+      playbackInstance
+    });
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>Aloha Music</Text>
+        <Image source={require("./images/ukulele.png")} style={styles.image} />
+        <View>
+          <TouchableOpacity onPress={this.handlePlayPause}>
+            {this.state.isPlaying ? (
+              <Feather name="pause" size={32} color="#563822" />
+            ) : (
+              <Feather name="play" size={32} color="#563822" />
+            )}
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f4e3cf",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    width: 300,
+    backgroundColor: "#da9547",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  image: {
+    width: 300,
+    height: 500,
+    margin: 40,
   },
 });
